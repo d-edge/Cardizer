c6f2f484a52b91c1afe8c19ba3e66b78d446a71f
adegouvello <adegouvello@d-edge.com>
2021-03-12 20:50:25 +0100

    refactor: improve code quality

--- src/Cardizer/Cardizer.fs
+++ src/Cardizer/Cardizer.fs
@@ -3,9 +3,9 @@
 open System
 open System.Threading
 
+// original snippet by @tpetricek
 // https://stackoverflow.com/a/7792667/1248177
-let next =
-    // Create master seed generator and thread local value
+let private next =
     let seedGenerator = Random()
 
     let localGenerator =
@@ -15,24 +15,24 @@ let next =
                 (fun _ ->
                     let seed = seedGenerator.Next()
                     Random(seed)))
-    // Return function that uses thread local random generator
+
     fun n -> localGenerator.Value.Next(n)
 
 let private nextDigit () = next 10
 
-let private getVisaSum n i =
+let private sumDigit n i =
     let n2 = n * 2
 
     if i % 2 = 0 then n
     else if n2 > 9 then n2 - 9
     else n2
 
-let rec private getVisaNumbers digits sum i =
-    if i = 14 then
-        (digits, sum)
+let rec private getVisaNumbers length digits sum i =
+    if i = length then
+        (sum, digits)
     else
         let n = nextDigit ()
-        getVisaNumbers (digits @ [ n ]) (sum + (getVisaSum n i)) (i + 1)
+        getVisaNumbers length (digits @ [ n ]) (sum + (sumDigit n i)) (i + 1)
 
 let inline private checkDigit sum = ((sum / 10 + 1) * 10 - sum) % 10
 
@@ -41,6 +41,6 @@ let private digitsToCard sum digits =
     |> List.fold (fun r n -> r + (string n)) String.Empty
 
 let generateVisa () =
-    let digits, sum = getVisaNumbers [ 4 ] 8 0
+    let sum, digits = getVisaNumbers 14 [ 4 ] 8 0
 
     digitsToCard sum digits
