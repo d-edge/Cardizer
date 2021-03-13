df3e9190a28e72f18af5e179369a35dfa0af9298
adegouvello <adegouvello@d-edge.com>
2021-03-13 19:26:50 +0100

    refactor: remove unneeded iterator

--- src/Dedge.Cardizer/Dedge.Cardizer.fs
+++ src/Dedge.Cardizer/Dedge.Cardizer.fs
@@ -20,19 +20,19 @@ let private next =
 
 let private nextDigit () = next 10
 
-let private sumDigit n i =
+let private sumDigit n length =
     let n2 = n * 2
 
-    if i % 2 = 0 then n
+    if length % 2 = 0 then n
     else if n2 > 9 then n2 - 9
     else n2
 
-let rec private getVisaNumbers length digits sum i =
-    if i = length then
+let rec private getNumbers digits sum length =
+    if length = 0 then
         (sum, digits)
     else
         let n = nextDigit ()
-        getVisaNumbers length (digits @ [ n ]) (sum + (sumDigit n i)) (i + 1)
+        getNumbers (digits @ [ n ]) (sum + (sumDigit n length)) (length - 1)
 
 let inline private checkDigit sum = ((sum / 10 + 1) * 10 - sum) % 10
 
@@ -41,6 +41,6 @@ let private digitsToCard sum digits =
     |> List.fold (fun r n -> r + (string n)) String.Empty
 
 let generateVisa () =
-    let sum, digits = getVisaNumbers 14 [ 4 ] 8 0
+    let sum, digits = getNumbers [ 4 ] 8 14
 
     digitsToCard sum digits
