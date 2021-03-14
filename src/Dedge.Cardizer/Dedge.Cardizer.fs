6ecf34de7d06430c0cf9fa1e145345b57df0ebdd
adegouvello <adegouvello@d-edge.com>
2021-03-14 13:04:45 +0100

    refactor: embrace mapFold

--- src/Dedge.Cardizer/Dedge.Cardizer.fs
+++ src/Dedge.Cardizer/Dedge.Cardizer.fs
@@ -20,39 +20,17 @@ let private next =
 
 let private nextDigit () = next 10
 
-let private sumDigit n length =
+let private getNumber n =
     let n2 = n * 2
+    if n2 > 9 then n2 - 9 else n2
 
-    if length % 2 = 0 then n
-    else if n2 > 9 then n2 - 9
-    else n2
+let private sumDigit n i =
+    if i % 2 = 0 then n else getNumber n
 
-// let rec private getNumbers digits sum length =
-//     if length = 0 then
-//         (sum, digits)
-//     else
-//         let n = nextDigit ()
-//         getNumbers (digits @ [ n ]) (sum + (sumDigit n length)) (length - 1)
-
-// let private getNumbers2 digits sum length =
-//     let numbers =
-//         [ 0 .. length - 1 ]
-//         |> List.map (fun _ -> nextDigit ())
-
-//     let result =
-//         numbers
-//         |> List.mapi (fun i n -> (i, n))
-//         |> List.fold (fun acc (i, n)-> acc + (sumDigit n i)) sum
-    
-//     (result, digits @ numbers)
-
-let private getNumbers3 digits sum length =
-    let numbers, result =
-        [ 0 .. length - 1 ]
-        |> List.map (fun i -> (i, nextDigit ()))
-        |> List.mapFold (fun acc (i, n) -> (n, acc + (sumDigit n i))) sum
-
-    (result, digits @ numbers)
+let private getNumbers state length =
+    [ 0 .. length - 1 ]
+    |> List.map (fun i -> (i, nextDigit ()))
+    |> List.mapFold (fun sum (i, n) -> (n, sum + (sumDigit n i))) state
 
 let inline private checkDigit sum = ((sum / 10 + 1) * 10 - sum) % 10
 
@@ -61,6 +39,6 @@ let private digitsToCard sum digits =
     |> List.fold (fun r n -> r + (string n)) String.Empty
 
 let generateVisa () =
-    let sum, digits = getNumbers3 [ 4 ] 8 14
+    let digits, sum = getNumbers 8 14
 
-    digitsToCard sum digits
+    digitsToCard sum (4 :: digits)
