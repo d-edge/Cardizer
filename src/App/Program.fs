8a05138f94e615001c09ee61f8c8b8990d02a8d2
adegouvello <adegouvello@d-edge.com>
2021-03-17 23:33:26 +0100

    feat(app): add verve and mir

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -1,10 +1,10 @@
-open System
-
 [<EntryPoint>]
 let main _ =
-    Dedge.Cardizer.generateVisa () |> printfn "Visa:\t\t%s"
-    Dedge.Cardizer.generateJcb () |> printfn "Jcb:\t\t%s"
     Dedge.Cardizer.generateAmex () |> printfn "Amex:\t\t%s"
     Dedge.Cardizer.generateDiscover () |> printfn "Discover:\t%s"
+    Dedge.Cardizer.generateJcb () |> printfn "Jcb:\t\t%s"
     Dedge.Cardizer.generateMasterCard () |> printfn "MasterCard:\t%s"
+    Dedge.Cardizer.generateMir () |> printfn "Mir:\t\t%s"
+    Dedge.Cardizer.generateVerve () |> printfn "Verve:\t\t%s"
+    Dedge.Cardizer.generateVisa () |> printfn "Visa:\t\t%s"
     0
