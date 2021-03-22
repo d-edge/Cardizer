68a6360de0e7244136435f34e98ffc308ff74f37
adegouvello <adegouvello@d-edge.com>
2021-03-22 12:09:55 +0100

    docs(appcs): add a C# example

---
+++ src/AppCs/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AppCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var card = Dedge.Cardizer.generateVisa();
+            Console.WriteLine(card);
+        }
+    }
+}
