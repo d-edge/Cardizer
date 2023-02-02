90e500087d77d27684740e059c480349e64147bf
d-edge automation <>
2023-02-02 16:48:50 +0000

    Automated changes

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -3,11 +3,11 @@ open DEdge
 [<EntryPoint>]
 let main _ =
     let cardizer = new Cardizer()
-    cardizer.NextAmex () |> printfn "Amex:\t\t%s"
-    cardizer.NextDiscover () |> printfn "Discover:\t%s"
-    cardizer.NextJcb () |> printfn "Jcb:\t\t%s"
-    cardizer.NextMasterCard () |> printfn "MasterCard:\t%s"
-    cardizer.NextMir () |> printfn "Mir:\t\t%s"
-    cardizer.NextVerve () |> printfn "Verve:\t\t%s"
-    cardizer.NextVisa () |> printfn "Visa:\t\t%s"
+    cardizer.NextAmex() |> printfn "Amex:\t\t%s"
+    cardizer.NextDiscover() |> printfn "Discover:\t%s"
+    cardizer.NextJcb() |> printfn "Jcb:\t\t%s"
+    cardizer.NextMasterCard() |> printfn "MasterCard:\t%s"
+    cardizer.NextMir() |> printfn "Mir:\t\t%s"
+    cardizer.NextVerve() |> printfn "Verve:\t\t%s"
+    cardizer.NextVisa() |> printfn "Visa:\t\t%s"
     0
